5715e3a230e5b964396e3f5fb49da3844838b991
Saumya <surasaumya17@gmail.com>
2024-07-27 20:52:37 +0530

    Home page done

---
+++ lib/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('2 in 1'),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          }
+        ),
+      ),
+    
+      body: Center(
+        child: Text('Home Page Content'),
+      ),
+    );
+  }
+}
